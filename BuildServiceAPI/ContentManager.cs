c380fd8554e47b3f824bfd0f91a20f2fc758f465
kate <kate@dariox.club>
2022-07-18 14:56:57 +0800

    Add ContentManager

---
+++ BuildServiceAPI/ContentManager.cs
@@ -0,0 +1,44 @@
+﻿using Minalyze.Shared.AutoUpdater;
+using Minalyze.Shared.Helpers;
+
+namespace BuildServiceAPI
+{
+    public class ContentManager
+    {
+        public List<ReleaseInfo> ReleaseInfoContent;
+        public Dictionary<string, ProductRelease> Releases = new Dictionary<string, ProductRelease>();
+        public Dictionary<string, PublishedRelease> Published = new Dictionary<string, PublishedRelease>();
+
+        internal int DatabaseVersion;
+
+        private string DATABASE_FILENAME = Path.Join(
+            Directory.GetCurrentDirectory(),
+            @"content.db");
+
+        public ContentManager()
+        {
+            databaseDeserialize();
+        }
+
+        private void databaseDeserialize()
+        {
+            DatabaseHelper.Read(DATABASE_FILENAME, sr =>
+            {
+                DatabaseVersion = sr.ReadInt32();
+                ReleaseInfoContent = (List<ReleaseInfo>)sr.ReadBList<ReleaseInfo>();
+                Releases = (Dictionary<string, ProductRelease>)sr.ReadDictionary<string, ProductRelease>();
+                Published = (Dictionary<string, PublishedRelease>)sr.ReadDictionary<string, PublishedRelease>();
+            });
+        }
+        public void DatabaseSerialize()
+        {
+            DatabaseHelper.Write(DATABASE_FILENAME, sw =>
+            {
+                sw.Write(DatabaseVersion);
+                sw.Write<ReleaseInfo>(ReleaseInfoContent);
+                sw.Write(Releases);
+                sw.Write(Published);
+            });
+        }
+    }
+}
