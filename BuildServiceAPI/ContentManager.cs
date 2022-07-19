5f1962282d76003085eda9c333337ce5a1347aba
kate <kate@dariox.club>
2022-07-19 14:54:28 +0800

    Add logic for downloading and uploading data to Google Firestore

--- BuildServiceAPI/ContentManager.cs
+++ BuildServiceAPI/ContentManager.cs
@@ -1,4 +1,5 @@
-﻿using Minalyze.Shared.AutoUpdater;
+﻿using Google.Cloud.Firestore;
+using Minalyze.Shared.AutoUpdater;
 using Minalyze.Shared.Helpers;
 
 namespace BuildServiceAPI
@@ -8,6 +9,8 @@ namespace BuildServiceAPI
         public List<ReleaseInfo> ReleaseInfoContent = new();
         public Dictionary<string, ProductRelease> Releases = new();
         public Dictionary<string, PublishedRelease> Published = new();
+        internal List<string> LoadedFirebaseAssets = new();
+        internal FirestoreDb database;
 
         internal int DatabaseVersion;
 
@@ -17,8 +20,119 @@ namespace BuildServiceAPI
 
         public ContentManager()
         {
-            databaseDeserialize();
+            database = FirestoreDb.Create(@"cloudtesting-3d734");
+            LoadFirebase();
         }
+        static ContentManager()
+        {
+            FirebaseHelper.FirebaseCollection.Add(typeof(PublishedRelease), "PublishedRelease");
+            FirebaseHelper.FirebaseCollection.Add(typeof(PublishedReleaseFile), "PublishedReleaseFile");
+        }
+
+        public void SaveFirebase()
+        {
+            Console.WriteLine($"[ContentManager->SaveFirebase] Uploading Content to Firebase");
+            var startTimestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            var count = 0;
+            foreach (var item in ReleaseInfoContent)
+            {
+                item.ToFirebase(item.GetFirebaseDocumentReference(database));
+                count++;
+            }
+            foreach (var pair in Releases)
+            {
+                pair.Value.ToFirebase(pair.Value.GetFirebaseDocumentReference(database));
+                count++;
+                count += pair.Value.Streams.Length * 2;
+            }
+            foreach (var pair in Published)
+            {
+                pair.Value.ToFirebase(pair.Value.GetFirebaseDocumentReference(database));
+                count++;
+                count += pair.Value.Files.Length + 1;
+            }
+            Console.WriteLine($"[ContentManager->SaveFirebase] Uploaded {count} items in {DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - startTimestamp}ms");
+        }
+        public void LoadFirebase()
+        {
+            Console.WriteLine($"[ContentManager->LoadFirebase] Fetching Content from Firebase");
+            var startTimestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            var count = 0;
+            foreach (var item in ReleaseInfoContent)
+            {
+                var snapshot = item.GetFirebaseDocumentReference(database).GetSnapshotAsync().Result;
+                item.FromFirebase(snapshot);
+                count++;
+            }
+            foreach (var pair in Releases)
+            {
+                var snapshot = pair.Value.GetFirebaseDocumentReference(database).GetSnapshotAsync().Result;
+                pair.Value.FromFirebase(snapshot);
+                count++;
+                count += pair.Value.Streams.Length * 2;
+            }
+            foreach (var pair in Published)
+            {
+                var snapshot = pair.Value.GetFirebaseDocumentReference(database).GetSnapshotAsync().Result;
+                pair.Value.FromFirebase(snapshot);
+                count++;
+                count += pair.Value.Files.Length + 1;
+            }
+
+            var newReleaseInfoContent = new List<ReleaseInfo>();
+            var newReleases = new Dictionary<string, ProductRelease>();
+            var newPublished = new Dictionary<string, PublishedRelease>();
+
+            var collectionListAsync = database.ListRootCollectionsAsync().ToListAsync().Result;
+            foreach (var collection in collectionListAsync)
+            {
+                if (
+                collection.Path.EndsWith(FirebaseHelper.FirebaseCollection[typeof(ReleaseInfo)])
+                || collection.Path.EndsWith(FirebaseHelper.FirebaseCollection[typeof(ProductRelease)])
+                || collection.Path.EndsWith(FirebaseHelper.FirebaseCollection[typeof(PublishedRelease)]))
+                {
+                    var documents = collection.GetSnapshotAsync().Result;
+                    foreach (var doc in documents)
+                    {
+                        if (LoadedFirebaseAssets.Contains(doc.Reference.Path))
+                            continue;
+
+                        switch(doc.Reference.Path.Split("/documents/")[1].Split("/")[0])
+                        {
+                            case "Release":
+                                var rel = new ReleaseInfo();
+                                rel.FromFirebase(doc);
+                                newReleaseInfoContent.Add(rel);
+                                LoadedFirebaseAssets.Add(doc.Reference.Path);
+                                count++;
+                                break;
+                            case "ProductRelease":
+                                var prodrel = new ProductRelease();
+                                prodrel.FromFirebase(doc);
+                                newReleases.Add(prodrel.ProductID, prodrel);
+                                LoadedFirebaseAssets.Add(doc.Reference.Path);
+                                count++;
+                                count += prodrel.Streams.Length * 2;
+                                break;
+                            case "PublishedRelease":
+                                var pubrel = new PublishedRelease();
+                                pubrel.FromFirebase(doc);
+                                newPublished.Add(pubrel.CommitHash, pubrel);
+                                LoadedFirebaseAssets.Add(doc.Reference.Path);
+                                count++;
+                                count += pubrel.Files.Length;
+                                break;
+                        }
+                    }
+                }
+            }
+
+            Releases = new(Releases.Concat(newReleases));
+            Published = new(Published.Concat(newPublished));
+            ReleaseInfoContent = new(ReleaseInfoContent.Concat(newReleaseInfoContent));
+            Console.WriteLine($"[ContentManager->LoadFirebase] Fetched {count} items in {DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - startTimestamp}ms");
+        }
+
 
         private void databaseDeserialize()
         {
