6090a0de4ba4e8891d23935d020a6bbfe52ab603
kate! <kate@dariox.club>
2022-07-31 19:48:45 +0800

    Create interface ITokenGranter

---
+++ BuildServiceAPI/ITokenGranter.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildServiceAPI
+{
+    public interface ITokenGranter
+    {
+        public HttpClient httpClient { get; }
+        public bool Grant(string username, string password);
+    }
+}
