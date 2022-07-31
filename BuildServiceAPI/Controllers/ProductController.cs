b85c4518cd960bee54df193c8e55b302bc154443
kate! <kate@dariox.club>
2022-07-31 19:20:36 +0800

    Create ProductController

---
+++ BuildServiceAPI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuildServiceAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        [Route("available")]
+        public ActionResult FetchAvailable() => FetchAvailable("");
+
+        public ActionResult FetchAvailable(string token)
+        {
+            var productIDList = new List<string>();
+
+            return Json(productIDList, MainClass.serializerOptions);
+        }
+    }
+}
