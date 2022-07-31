cf04c78abc11fb2189829e9055b510a6c49e978a
kate! <kate@dariox.club>
2022-07-31 19:27:26 +0800

    Create TokenController

---
+++ BuildServiceAPI/Controllers/TokenController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BuildServiceAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public string Grant(string username, string password)
+        {
+            var isValid = false;
+            foreach (var validator in MainClass.TokenGrantList)
+            {
+                var res = validator.Grant(username, password);
+                if (res)
+                {
+                    isValid = true;
+                    break;
+                }
+            }
+
+            if (!isValid)
+            {
+                Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return "";
+            }
+        }
+    }
+}
