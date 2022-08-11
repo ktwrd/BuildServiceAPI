80e89d46130e60dd280611559e4eeeb7041685bc
kate <kate@dariox.club>
2022-08-11 13:38:19 +0800

    Create User Administration Endpoint Controller

---
+++ BuildServiceAPI/Controllers/Admin/User.cs
@@ -0,0 +1,88 @@
+﻿using BuildServiceCommon;
+using BuildServiceCommon.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace BuildServiceAPI.Controllers.Admin
+{
+
+    [Route("admin/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        public static bool IsAllowed(string token)
+        {
+            if (token == null || token.Length < 32 || token.Length > 32)
+            {
+                return false;
+            }
+
+            var account = MainClass.contentManager.AccountManager.GetAccount(token);
+            if (account == null)
+            {
+                return false;
+            }
+
+            return IsAllowed(account);
+        }
+        public static bool IsAllowed(Account account) => account.HasPermission(AccountPermission.ADMINISTRATOR);
+
+        [HttpGet]
+        [Route("list")]
+        public ActionResult List(string token, string username=null, SearchMethod usernameSearchType = SearchMethod.Equals, long firstSeenTimestamp=0, long lastSeenTimestamp=long.MaxValue)
+        {
+            if (!IsAllowed(token))
+            {
+                Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return Json(new ObjectResponse<string>()
+                {
+                    Success = false,
+                    Data = "Invalid Account"
+                });
+            }
+
+            var detailList = new List<AccountDetailsResponse>();
+            foreach (var account in MainClass.contentManager.AccountManager.AccountList)
+            {
+                if (account.FirstSeenTimestamp >= firstSeenTimestamp
+                    && account.LastSeenTimestamp <= lastSeenTimestamp)
+                {
+                    if (username == null)
+                        detailList.Add(account.GetDetails());
+                    else
+                    {
+                        bool cont = false;
+                        switch (usernameSearchType)
+                        {
+                            case SearchMethod.Equals:
+                                cont = username == account.Username;
+                                break;
+                            case SearchMethod.StartsWith:
+                                cont = account.Username.StartsWith(username);
+                                break;
+                            case SearchMethod.EndsWith:
+                                cont = account.Username.EndsWith(username);
+                                break;
+                            case SearchMethod.Includes:
+                                cont = account.Username.Contains(username);
+                                break;
+                            case SearchMethod.IncludesNot:
+                                cont = !account.Username.Contains(username);
+                                break;
+                        }
+                        if (cont)
+                            detailList.Add(account.GetDetails());
+                    }
+                }
+            }
+            return Json(new ObjectResponse<AccountDetailsResponse[]>()
+            {
+                Success = true,
+                Data = detailList.ToArray()
+            });
+        }
+    }
+}
