ff38b741cd0e509f2f9d590bf07bafbfb46e99cc
kate <kate@dariox.club>
2022-08-11 13:15:41 +0800

    Create class GrantTokenResponse

---
+++ BuildServiceCommon/Authorization/GrantTokenResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildServiceCommon.Authorization
+{
+    public class GrantTokenResponse
+    {
+        public GrantTokenResponse(string message = "", bool success = false, AccountToken token = null)
+        {
+            Message = message;
+            Token = token;
+            Success = success;
+        }
+        public bool Success { get; private set; }
+        public string Message { get; private set; }
+
+        /// <summary>
+        /// Nullable
+        /// </summary>
+        public AccountToken Token { get; private set; }
+    }
+}
