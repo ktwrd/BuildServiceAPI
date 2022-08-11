a31001946b12c74d643eb526a10a0bb9127f5b3b
kate <kate@dariox.club>
2022-08-11 13:38:01 +0800

    Create file EndpointShared.cs

---
+++ BuildServiceCommon/EndpointShared.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildServiceCommon
+{
+    public enum SearchMethod
+    {
+        Equals,
+        StartsWith,
+        EndsWith,
+        Includes,
+        IncludesNot
+    }
+}
