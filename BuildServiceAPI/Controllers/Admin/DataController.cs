32fad47f4d7acfdbc2ef9ca946491ac2759fe95b
kate <kate@dariox.club>
2022-08-12 14:53:24 +0800

    Add endpoint /admin/dump/setdata

--- BuildServiceAPI/Controllers/Admin/DataController.cs
+++ BuildServiceAPI/Controllers/Admin/DataController.cs
@@ -3,8 +3,10 @@ using BuildServiceCommon.Authorization;
 using BuildServiceCommon.AutoUpdater;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Text.Json;
 
 namespace BuildServiceAPI.Controllers.Admin
 {
@@ -70,6 +72,64 @@ namespace BuildServiceAPI.Controllers.Admin
                     Data = "Invalid parameter \"type\""
                 }, MainClass.serializerOptions);
             }
+        }
+        [HttpGet("setdata")]
+        public ActionResult SetData(string token, string content)
+        {
+            if (!MainClass.contentManager.AccountManager.AccountHasPermission(token, AccountPermission.ADMINISTRATOR))
+            {
+                Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return Json(new ObjectResponse<string>()
+                {
+                    Success = false,
+                    Data = "Invalid Permissions"
+                }, MainClass.serializerOptions);
+            }
+            var deserializedDynamic = JsonSerializer.Deserialize<ObjectResponse<dynamic>>(WebUtility.UrlDecode(content), MainClass.serializerOptions);
+            Type targetType = Type.GetType(deserializedDynamic.DataType);
+            bool success = false;
+            if (targetType == typeof(ReleaseInfo[]))
+            {
+                MainClass.contentManager.ReleaseInfoContent = new List<ReleaseInfo>(deserializedDynamic.Data);
+                success = true;
+            }
+            else if (targetType == MainClass.contentManager.Releases.GetType())
+            {
+                MainClass.contentManager.Releases = deserializedDynamic.Data;
+                success = true;
+            }
+            else if (targetType == MainClass.contentManager.Published.GetType())
+            {
+                MainClass.contentManager.Published = deserializedDynamic.Data;
+                success = true;
+            }
+            else if (targetType == typeof(AllDataResult))
+            {
+                var c = (AllDataResult)deserializedDynamic.Data;
+                MainClass.contentManager.ReleaseInfoContent = new List<ReleaseInfo>(c.ReleaseInfoContent);
+                MainClass.contentManager.Releases = c.Releases;
+                MainClass.contentManager.Published = c.Published;
+                success = true;
+            }
+
+            if (success)
+            {
+                MainClass.contentManager.DatabaseSerialize();
+                return Json(new ObjectResponse<object>()
+                {
+                    Success = true,
+                    Data = deserializedDynamic.Data
+                }, MainClass.serializerOptions);
+            }
+            else
+            {
+                return Json(new ObjectResponse<string>()
+                {
+                    Success = false,
+                    Data = "Invalid parameter \"type\""
+                }, MainClass.serializerOptions);
+            }
+
         }
     }
 }
