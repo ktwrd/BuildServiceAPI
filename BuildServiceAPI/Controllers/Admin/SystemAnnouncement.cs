f771549e4b9653e97fce5a60e8bc98c8db88520e
kate <kate@dariox.club>
2022-08-12 12:31:39 +0800

    Add endpoint /admin/announcement/setData

--- BuildServiceAPI/Controllers/Admin/SystemAnnouncement.cs
+++ BuildServiceAPI/Controllers/Admin/SystemAnnouncement.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Net;
-
+using System.Text.Json;
 
 namespace BuildServiceAPI.Controllers.Admin
 {
@@ -110,6 +110,41 @@ namespace BuildServiceAPI.Controllers.Admin
             {
                 Success = true,
                 Data = MainClass.contentManager.SystemAnnouncement.GetSummary()
+            }, MainClass.serializerOptions);
+        }
+
+    
+        [HttpGet]
+        [Route("setData")]
+        public ActionResult SetData(string token, string content)
+        {
+            if (!MainClass.contentManager.AccountManager.AccountHasPermission(token, AccountPermission.ADMINISTRATOR))
+            {
+                Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return Json(new ObjectResponse<string>()
+                {
+                    Success = false,
+                    Data = "Invalid Permissions"
+                }, MainClass.serializerOptions);
+            }
+
+            var attemptedDeserialized = JsonSerializer.Deserialize<SystemAnnouncementSummary>(content, MainClass.serializerOptions);
+            if (attemptedDeserialized == null) {
+                Response.StatusCode = (int)HttpStatusCode.ServiceUnavailable;
+                return Json(new ObjectResponse<string>()
+                {
+                    Success = false,
+                    Data = "Attempted to deserialized, but it failed (returned null)"
+                }, MainClass.serializerOptions);
+            }
+
+            MainClass.contentManager.SystemAnnouncement.Read(WebUtility.UrlDecode(content));
+            MainClass.contentManager.SystemAnnouncement.OnUpdate();
+
+            return Json(new ObjectResponse<SystemAnnouncementSummary>()
+            {
+                Success = true,
+                Data = attemptedDeserialized
             }, MainClass.serializerOptions);
         }
     }
