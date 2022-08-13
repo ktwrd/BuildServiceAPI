b31f26635e8d0512348b42e3168fa3c7e31e8856
kate <kate@dariox.club>
2022-08-13 22:26:04 +0800

    Add setting ShowLatestRelease

--- BuildServiceAPI.DesktopClient/Properties/Settings.Designer.cs
+++ BuildServiceAPI.DesktopClient/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace BuildServiceAPI.DesktopClient.Properties {
                 this["ServerEndpoint"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool ShowLatestRelease {
+            get {
+                return ((bool)(this["ShowLatestRelease"]));
+            }
+            set {
+                this["ShowLatestRelease"] = value;
+            }
+        }
     }
 }
