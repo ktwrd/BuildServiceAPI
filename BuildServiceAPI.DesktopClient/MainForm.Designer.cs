8557a96ca4ebf6d77c5959b60288bd6b8cce37ee
kate <kate@dariox.club>
2022-08-15 17:04:17 +0800

    Set sizes

--- BuildServiceAPI.DesktopClient/MainForm.Designer.cs
+++ BuildServiceAPI.DesktopClient/MainForm.Designer.cs
@@ -35,10 +35,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
-            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
             this.Name = "MainForm";
             this.Text = "Build Server Client";
             this.ResumeLayout(false);
