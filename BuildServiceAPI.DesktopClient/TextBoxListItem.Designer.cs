065969dc4c37d48181ac93175cb41f47af19b439
kate <kate@dariox.club>
2022-08-15 17:04:29 +0800

    Create Component TextBoxListItem

---
+++ BuildServiceAPI.DesktopClient/TextBoxListItem.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BuildServiceAPI.DesktopClient
+{
+    partial class TextBoxListItem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextBoxListItem));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 64F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.AddColumns;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(150, 35);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(150, 35);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.buttonAdd);
+            this.flowLayoutPanel1.Controls.Add(this.buttonRemove);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(89, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(58, 29);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Image = ((System.Drawing.Image)(resources.GetObject("buttonAdd.Image")));
+            this.buttonAdd.Location = new System.Drawing.Point(3, 3);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(23, 23);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Image = ((System.Drawing.Image)(resources.GetObject("buttonRemove.Image")));
+            this.buttonRemove.Location = new System.Drawing.Point(32, 3);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(23, 23);
+            this.buttonRemove.TabIndex = 1;
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.richTextBox1.Location = new System.Drawing.Point(6, 6);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(74, 23);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // TextBoxListItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(150, 35);
+            this.Name = "TextBoxListItem";
+            this.Size = new System.Drawing.Size(150, 35);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ BuildServiceAPI.DesktopClient/TextBoxListItem.cs
@@ -0,0 +1,104 @@
+﻿using kate.shared.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuildServiceAPI.DesktopClient
+{
+    public partial class TextBoxListItem : UserControl
+    {
+        public TextBoxListItem()
+        {
+            InitializeComponent();
+
+            richTextBox1.KeyDown += RichTextBox1_KeyDown;
+            richTextBox1.KeyPress += RichTextBox1_KeyPress;
+            richTextBox1.KeyUp += RichTextBox1_KeyUp;
+            buttonAdd.Click += ButtonAdd_Click;
+            buttonRemove.Click += ButtonRemove_Click;
+            richTextBox1.TextChanged += RichTextBox1_TextChanged;
+            richTextBox1.Enter += RichTextBox1_Enter;
+            richTextBox1.GotFocus += RichTextBox1_GotFocus;
+        }
+
+        private void RichTextBox1_GotFocus(object sender, EventArgs e) => TextBoxGotFocus?.Invoke(sender, e);
+
+        private void RichTextBox1_Enter(object sender, EventArgs e) => TextBoxEnter?.Invoke(sender, e);
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e) => TextChanged?.Invoke(sender, e);
+
+        private void ButtonRemove_Click(object sender, EventArgs e) => RemoveButtonClick?.Invoke();
+
+        private void ButtonAdd_Click(object sender, EventArgs e) => AddButtonClick?.Invoke();
+
+        private void RichTextBox1_KeyUp(object sender, KeyEventArgs e) => TextBoxKeyUp?.Invoke(sender, e);
+
+        private void RichTextBox1_KeyPress(object sender, KeyPressEventArgs e) => TextBoxKeyPress?.Invoke(sender, e);
+
+        private void RichTextBox1_KeyDown(object sender, KeyEventArgs e) => TextBoxKeyDown?.Invoke(sender, e);
+
+        public object ObjectData = null;
+
+        public bool ButtonAddEnable
+        {
+            get { return buttonAdd.Enabled; }
+            set { buttonAdd.Enabled = value; }
+        }
+        public bool ButtonRemoveEnable
+        {
+            get { return buttonRemove.Enabled; }
+            set { buttonRemove.Enabled = value; }
+        }
+
+        [Localizable(true)]
+        [RefreshProperties(RefreshProperties.All)]
+        public new string Text
+        {
+            get { return richTextBox1.Text; }
+            set { richTextBox1.Text = value; }
+        }
+        [Category("CatAppearance")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        [MergableProperty(false)]
+        [Localizable(true)]
+        [Description("TextBoxLinesDescr")]
+        [Editor("System.Windows.Forms.Design.StringArrayEditor, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
+        public string[] Lines
+        {
+            get { return richTextBox1.Lines; }
+            set { richTextBox1.Lines = value; }
+        }
+        private bool _readOnly = false;
+        [Category("CatBehavior")]
+        [DefaultValue(false)]
+        [RefreshProperties(RefreshProperties.Repaint)]
+        [Description("TextBoxReadOnlyDescr")]
+        public bool ReadOnly
+        {
+            get { return _readOnly; }
+            set
+            {
+                _readOnly = value;
+                richTextBox1.ReadOnly = true;
+            }
+        }
+
+        public event KeyEventHandler TextBoxKeyDown;
+        public event KeyPressEventHandler TextBoxKeyPress;
+        public event KeyEventHandler TextBoxKeyUp;
+
+        public event VoidDelegate AddButtonClick;
+        public event VoidDelegate RemoveButtonClick;
+
+        public new event EventHandler TextChanged;
+        public event EventHandler TextBoxEnter;
+        public event EventHandler TextBoxGotFocus;
+    }
+}
