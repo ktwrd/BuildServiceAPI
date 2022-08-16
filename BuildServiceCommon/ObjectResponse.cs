481f1cdad2ebce5fb843318feb4592cc4ddcfe36
kate <kate@dariox.club>
2022-08-16 10:47:13 +0800

    Use AssemblyQualifiedName

--- BuildServiceCommon/ObjectResponse.cs
+++ BuildServiceCommon/ObjectResponse.cs
@@ -7,7 +7,12 @@ namespace BuildServiceCommon
     public class ObjectResponse<T>
     {
         public bool Success { get; set; }
+        private T _data;
         public T Data { get; set; }
-        public string DataType => Data?.GetType().FullName;
+        public string DataType { get; set; }
+        public ObjectResponse()
+        {
+            DataType = typeof(T).AssemblyQualifiedName;
+        }
     }
 }
