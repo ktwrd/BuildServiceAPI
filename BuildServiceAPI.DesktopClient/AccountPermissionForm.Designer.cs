fdf4bdbf3814a589ec4c4babba5f54234417d310
kate <kate@dariox.club>
2022-08-31 14:29:42 +0800

    Create AccountPermissionForm

---
+++ BuildServiceAPI.DesktopClient/AccountPermissionForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace BuildServiceAPI.DesktopClient
+{
+    partial class AccountPermissionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBoxPermissions = new System.Windows.Forms.CheckedListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonPush = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxPermissions
+            // 
+            this.checkedListBoxPermissions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBoxPermissions.FormattingEnabled = true;
+            this.checkedListBoxPermissions.Location = new System.Drawing.Point(3, 3);
+            this.checkedListBoxPermissions.Name = "checkedListBoxPermissions";
+            this.checkedListBoxPermissions.Size = new System.Drawing.Size(258, 202);
+            this.checkedListBoxPermissions.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.checkedListBoxPermissions, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonPush, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(264, 258);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // buttonPush
+            // 
+            this.buttonPush.Location = new System.Drawing.Point(3, 211);
+            this.buttonPush.Name = "buttonPush";
+            this.buttonPush.Size = new System.Drawing.Size(75, 23);
+            this.buttonPush.TabIndex = 2;
+            this.buttonPush.Text = "Push";
+            this.buttonPush.UseVisualStyleBackColor = true;
+            this.buttonPush.Click += new System.EventHandler(this.buttonPush_Click);
+            // 
+            // AccountPermissionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 258);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "AccountPermissionForm";
+            this.Text = "Edit Account Permissions";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxPermissions;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonPush;
+    }
+}
---
+++ BuildServiceAPI.DesktopClient/AccountPermissionForm.cs
@@ -0,0 +1,95 @@
+﻿using BuildServiceCommon.Authorization;
+using kate.shared.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuildServiceAPI.DesktopClient
+{
+    public partial class AccountPermissionForm : Form
+    {
+        public AccountDetailsResponse Account;
+        public AdminForm AdminForm;
+        public AccountPermissionForm(AccountDetailsResponse account, AdminForm adminForm)
+        {
+            InitializeComponent();
+            Account = account;
+            AdminForm = adminForm;
+
+            UpdatePermissionCheckboxList();
+
+            for (int i = 0; i < checkedListBoxPermissions.Items.Count; i++)
+            {
+                var perm = (AccountPermission)checkedListBoxPermissions.Items[i];
+                if (new List<AccountPermission>(Account.Permissions).Contains(perm))
+                {
+                    checkedListBoxPermissions.SetItemChecked(i, true);    
+                }
+            }
+        }
+
+        public void UpdatePermissionCheckboxList()
+        {
+            //checkedListBoxPermissions
+            checkedListBoxPermissions.Items.Clear();
+            foreach (var item in GeneralHelper.GetEnumList<AccountPermission>())
+            {
+                checkedListBoxPermissions.Items.Add(item);
+            }
+        }
+
+        private void buttonPush_Click(object sender, EventArgs e)
+        {
+            Enabled = false;
+            AdminForm.Enabled = false;
+            var revokeList = new List<AccountPermission>();
+            var grantList = new List<AccountPermission>();
+            for (int i = 0; i < checkedListBoxPermissions.Items.Count; i++)
+            {
+                var target = (AccountPermission)checkedListBoxPermissions.Items[i];
+                bool is_checked = checkedListBoxPermissions.GetItemChecked(i);
+                bool previousState = (new List<AccountPermission>(Account.Permissions).Contains((AccountPermission)checkedListBoxPermissions.Items[i]));
+                if (is_checked && previousState == false)
+                {
+                    grantList.Add(target);
+                }
+                else if (is_checked == false && previousState == true)
+                {
+                    revokeList.Add(target);
+                }
+            }
+
+            var taskList = new List<Task>();
+            foreach (var perm in revokeList)
+            {
+                taskList.Add(new Task(delegate
+                {
+                    var targetURL = Endpoint.UserPermissionRevoke(AdminForm.Token.Token, Account.Username, perm);
+                    AdminForm.httpClient.GetAsync(targetURL).Wait();
+                }));
+            }
+            foreach (var perm in grantList)
+            {
+                taskList.Add(new Task(delegate
+                {
+                    var targetURL = Endpoint.UserPermissionGrant(AdminForm.Token.Token, Account.Username, perm);
+                    AdminForm.httpClient.GetAsync(targetURL).Wait();
+                }));
+            }
+            foreach (var t in taskList)
+                t.Start();
+            Task.WhenAll(taskList);
+            Enabled = true;
+            AdminForm.RefreshAccounts();
+            AdminForm.RefreshAccountListView();
+            AdminForm.Enabled = true;
+            Close();
+        }
+    }
+}
