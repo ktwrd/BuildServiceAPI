b41ab65e1ce4f13a5853a9bfe6a7483327f15ca1
kate <kate@dariox.club>
2022-09-06 11:13:03 +0800

    Create build scripts :3

---
+++ BuildServiceAPI.DesktopClient/BuildScripts/Build-Compile.ps1
@@ -0,0 +1,19 @@
+. $PSScriptRoot\Shared.ps1
+Include-VisualStudioTools
+
+$env:BuildScriptDirectory=$pwd;
+Set-Location ../
+$env:TargetProjectDirectory=$pwd;
+
+Write-Output "================================================================ Restoring Packages"
+msbuild /t:Restore  /p:Configuration=Release -maxcpucount:4
+Write-Output "================================================================ Compiling"
+msbuild /p:Configuration=Release -maxcpucount:4
+
+Remove-Item -Recurse -Force BuildScripts\release\
+mkdir BuildScripts\release\
+Copy-Item bin\${ILMERGE_BUILD}\* BuildScripts\release\
+
+# . $PSScriptRoot\Build-ILMerge.ps1
+
+Set-Location $env:BuildScriptDirectory
