c11c440b264274d5c9711e4cfdc5d176bd907154
kate! <kate@dariox.club>
2022-09-22 18:34:33 +0800

    Create static class ServerStringResponse

---
+++ BuildServiceAPI/ServerStringResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildServiceAPI
+{
+    public static class ServerStringResponse
+    {
+        public static string InvalidCredential = "Invalid Credential";
+        public static string AccountNotFound(string username)
+            => $"Could not find account with username of \"{username}\"";
+    }
+}
