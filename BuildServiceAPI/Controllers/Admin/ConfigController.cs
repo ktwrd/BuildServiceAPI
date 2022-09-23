bcade9f055bef9f5ba6024e1be864df10928dac6
kate <kate@dariox.club>
2022-09-23 15:41:11 +0800

    Change route admin/config/setvalue to HttpPost with body of config value

--- BuildServiceAPI/Controllers/Admin/ConfigController.cs
+++ BuildServiceAPI/Controllers/Admin/ConfigController.cs
@@ -39,9 +39,9 @@ namespace BuildServiceAPI.Controllers.Admin
             }, MainClass.serializerOptions);
         }
 
-        [HttpGet]
+        [HttpPost]
         [Route("setvalue")]
-        public ActionResult SetValue(string token, string group, string key, string value)
+        public ActionResult SetValue(string token, string group, string key)
         {
             if (!MainClass.contentManager.AccountManager.AccountHasPermission(token, RequiredPermissions))
             {
@@ -53,7 +53,38 @@ namespace BuildServiceAPI.Controllers.Admin
                 }, MainClass.serializerOptions);
             }
 
-            ServerConfig.Set(group, key, value);
+
+            var syncIOFeature = HttpContext.Features.Get<IHttpBodyControlFeature>();
+            if (syncIOFeature != null)
+            {
+                syncIOFeature.AllowSynchronousIO = true;
+            }
+            object? decodedBody;
+            try
+            {
+                StreamReader reader = new StreamReader(Request.Body);
+                string decodedBodyText = reader.ReadToEnd();
+                decodedBody = JsonSerializer.Deserialize<object>(decodedBodyText, MainClass.serializerOptions);
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = 401;
+                return Json(new ObjectResponse<HttpException>()
+                {
+                    Success = false,
+                    Data = new HttpException(401, "Invalid Body", e)
+                }, MainClass.serializerOptions);
+            }
+
+            ServerConfig.Set(group, key, decodedBody ?? "");
+
+            return Json(new ObjectResponse<Dictionary<string, Dictionary<string, object>>>()
+            {
+                Data = ServerConfig.Get(),
+                Success = true
+            }, MainClass.serializerOptions);
+        }
+
 
             return Json(new ObjectResponse<Dictionary<string, Dictionary<string, object>>>()
             {
