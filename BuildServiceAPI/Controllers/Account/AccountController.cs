75298a251d5dff9ed8ae2d579ab5c1858f6fd23c
kate! <kate@dariox.club>
2022-09-23 20:25:54 +0800

    Create Account endpoints

---
+++ BuildServiceAPI/Controllers/Account/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using BuildServiceCommon;
+using BuildServiceCommon.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BuildServiceAPI.Controllers.Account
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(string token)
+        {
+            var account = MainClass.contentManager.AccountManager.GetAccount(token ?? "");
+            if (account == null)
+            {
+                Response.StatusCode = 401;
+                return Json(new ObjectResponse<HttpException>()
+                {
+                    Success = false,
+                    Data = new HttpException(401, ServerStringResponse.InvalidCredential)
+                }, MainClass.serializerOptions);
+            }
+            if (!account.Enabled)
+            {
+                Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return Json(new ObjectResponse<HttpException>()
+                {
+                    Success = false,
+                    Data = new HttpException((int)HttpStatusCode.Unauthorized, ServerStringResponse.AccountDisabled)
+                }, MainClass.serializerOptions);
+            }
+
+            return Json(new ObjectResponse<AccountDetailsResponse>()
+            {
+                Success = true,
+                Data = account.GetDetails()
+            }, MainClass.serializerOptions);
+        }
+    }
+}
---
+++ BuildServiceAPI/Controllers/Account/PermissionsController.cs
@@ -0,0 +1,43 @@
+﻿using BuildServiceCommon;
+using BuildServiceCommon.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BuildServiceAPI.Controllers.Account
+{
+    [Route("account/[controller]")]
+    [ApiController]
+    public class PermissionsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(string token)
+        {
+            var account = MainClass.contentManager.AccountManager.GetAccount(token ?? "");
+            if (account == null)
+            {
+                Response.StatusCode = 401;
+                return Json(new ObjectResponse<HttpException>()
+                {
+                    Success = false,
+                    Data = new HttpException(401, ServerStringResponse.InvalidCredential)
+                }, MainClass.serializerOptions);
+            }
+            if (!account.Enabled)
+            {
+                Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return Json(new ObjectResponse<HttpException>()
+                {
+                    Success = false,
+                    Data = new HttpException((int)HttpStatusCode.Unauthorized, ServerStringResponse.AccountDisabled)
+                }, MainClass.serializerOptions);
+            }
+
+            return Json(new ObjectResponse<AccountPermission[]>()
+            {
+                Success = true,
+                Data = account.Permissions.ToArray()
+            }, MainClass.serializerOptions);
+        }
+    }
+}
