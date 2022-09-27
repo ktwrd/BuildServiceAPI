ef18f996f5ff1a76e6dbd07dff284ed2b12b5c08
kate! <kate@dariox.club>
2022-09-27 18:31:24 +0800

    Automatically generate Build Service API Version

---
+++ BuildServiceAPI/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Reflection;
+
+namespace BuildServiceAPI.Controllers
+{
+    [Route("version")]
+    [ApiController]
+    public class VersionController : Controller
+    {
+        public string ServerVersion()
+        {
+            var asm = Assembly.GetAssembly(typeof(VersionController));
+            if (asm == null)
+            {
+                return @"0.0.0.0";
+            }
+
+            Version? version = asm.GetName().Version;
+            
+            if (version == null)
+            {
+                return @"0.0.0.0";
+            }
+
+            return version.ToString();
+        }
+    }
+}
---
+++ BuildServiceAPI/Properties/VersionAutoIncrement.cs
@@ -0,0 +1,6 @@
+﻿
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.71.623")]
+[assembly: AssemblyFileVersion("1.0.71.623")]
+
