ea7bfa0ae746612e7786929601864fb80f7751d1
kate! <kate@dariox.club>
2022-09-28 16:56:04 +0800

    Create class ServerDetailsResponse

---
+++ BuildServiceCommon/ServerDetailsResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildServiceCommon
+{
+    public class ServerDetailsResponse
+    {
+        /// <summary>
+        /// Measured in seconds since the server was started.
+        /// </summary>
+        public long Uptime { get; set; }
+        public string Version { get; set; }
+    }
+}
