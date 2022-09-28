bfbf9b283b986bf138652bf08466f6790d66dcba
kate! <kate@dariox.club>
2022-09-28 14:56:49 +0800

    Update AccountPermission.cs

--- BuildServiceCommon/Authorization/AccountPermission.cs
+++ BuildServiceCommon/Authorization/AccountPermission.cs
@@ -9,13 +9,13 @@ namespace BuildServiceCommon.Authorization
         ADMINISTRATOR,
         CREATE_RELEASE,
         READ_RELEASE_BYPASS,
-        DELETE_RELEASE,
-        DELETE_ACCOUNT,
         CONFIG_READ,
         CONFIG_WRITE,
         USER_GROUP_MODIFY,
         USER_PERMISSION_MODIFY,
         USER_TOKEN_PURGE,
-        USER_LIST
+        USER_LIST,
+        ANNOUNCEMENT_MANAGE,
+        RELEASE_MANAGE
     }
 }
