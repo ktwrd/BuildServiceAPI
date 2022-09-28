1f98a76e2bade306464c9ccf62abcf6593ab3cee
kate! <kate@dariox.club>
2022-09-28 14:58:04 +0800

    Add route /admin/bot/list

---
+++ BuildServiceAPI/Controllers/Admin/Bot/BotListingController.cs
@@ -0,0 +1,32 @@
+﻿using BuildServiceCommon;
+using BuildServiceCommon.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BuildServiceAPI.Controllers.Admin.Bot
+{
+    [Route("admin/bot")]
+    [ApiController]
+    public class BotListingController : Controller
+    {
+        [HttpGet("list")]
+        public ActionResult ListBots(string token)
+        {
+            if (!MainClass.contentManager.AccountManager.AccountHasPermission(token, AccountPermission.ADMINISTRATOR))
+            {
+                Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return Json(new ObjectResponse<string>()
+                {
+                    Success = false,
+                    Data = ServerStringResponse.InvalidCredential
+                }, MainClass.serializerOptions);
+            }
+            return Json(new ObjectResponse<object>()
+            {
+                Success = true,
+                Data = new object()
+            }, MainClass.serializerOptions);
+        }
+    }
+}
