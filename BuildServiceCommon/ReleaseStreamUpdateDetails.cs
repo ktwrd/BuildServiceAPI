bc8d2fd8c47b430547b4d7771ed67096529525a0
kate! <kate@dariox.club>
2022-09-28 14:56:45 +0800

    Create class ReleaseStreamUpdateDetails

---
+++ BuildServiceCommon/ReleaseStreamUpdateDetails.cs
@@ -0,0 +1,22 @@
+﻿using BuildServiceCommon.AutoUpdater;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildServiceCommon
+{
+    public class ReleaseStreamUpdateDetails
+    {
+        /// <summary>
+        /// Nullable
+        /// </summary>
+        public ProductReleaseStream Stream { get; set; } = null;
+
+        public string TargetApp { get; set; } = "";
+        public string TargetBranch { get; set; } = "";
+        public string CurrentHash { get; set; } = "";
+
+        public long UpdateReleaseTimestamp { get; set; } = -1;
+        public bool HasUpdate { get; set; } = false;
+    }
+}
